7fbfdd4792bebad90905099aa6bfd81ea78dc134
im-a-robo <nmdasil514@pccsk12.com>
2022-06-07 17:55:11 -0400

    working with xbox controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.Spinnies;
 import frc.robot.subsystems.Wheelies;
@@ -13,13 +14,15 @@ public class RobotContainer {
     private final Joystick driverLeft = new Joystick(0);
     private final Joystick driverRight = new Joystick(1);
 
+    private final XboxController driver =  new XboxController(0);
+
     public RobotContainer() {
         configureButtonBindings();
         configureDefaultCommands();
     }
 
     public void configureDefaultCommands() {
-        wheelies.setDefaultCommand(new Spinnies(wheelies, () -> MathUtil.applyDeadband(driverLeft.getY(), 0.2), () -> MathUtil.applyDeadband(driverRight.getY(), 0.2)));
+        wheelies.setDefaultCommand(new Spinnies(wheelies, () -> -MathUtil.applyDeadband(driver.getLeftY(), 0.2) * 0.5, () -> -MathUtil.applyDeadband(driver.getRightY(), 0.2) * 0.5));
     }
 
     private void configureButtonBindings() {
